7aaebb4907a5f81e879bfa29bc3764df314ed01c
Greg Haskins <greg@manetu.com>
2019-12-29 11:47:05 -0500

    Run 'lein ancient'
    We purposely hold back jetty http2-client to the v9.x series since v10.x is
    still in -alpha
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- resources/leiningen/new/protojure/project.clj
+++ resources/leiningen/new/protojure/project.clj
@@ -5,31 +5,31 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :year 2019
             :key "apache-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.pedestal/pedestal.service "0.5.5"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.pedestal/pedestal.service "0.5.7"]
 
                  ;; -- PROTOC-GEN-CLOJURE --
-                 [protojure "1.0.0"]
+                 [protojure "1.0.1"]
 
                  ;; -- PROTOC_GEN_CLOJURE CLIENT DEP --
-                 [org.eclipse.jetty.http2/http2-client "9.4.17.v20190418"]
+                 [org.eclipse.jetty.http2/http2-client "9.4.25.v20191220"]
 
                  ;; Include Undertow for supporting HTTP/2 for GRPCs
-                 [io.undertow/undertow-core "2.0.25.Final"]
-                 [io.undertow/undertow-servlet "2.0.25.Final"]
+                 [io.undertow/undertow-core "2.0.28.Final"]
+                 [io.undertow/undertow-servlet "2.0.28.Final"]
                  ;; And of course, protobufs
-                 [com.google.protobuf/protobuf-java "3.9.1"]
+                 [com.google.protobuf/protobuf-java "3.11.1"]
                  ;; logging
                  [com.taoensso/timbre "4.10.0"]
-                 [com.fzakaria/slf4j-timbre "0.3.14"]
+                 [com.fzakaria/slf4j-timbre "0.3.17"]
 
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.25"]
-                 [org.slf4j/jcl-over-slf4j "1.7.25"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]]
+                 [org.slf4j/jul-to-slf4j "1.7.30"]
+                 [org.slf4j/jcl-over-slf4j "1.7.30"]
+                 [org.slf4j/log4j-over-slf4j "1.7.30"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "{{name}}.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.5"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.7"]]}
              :uberjar {:aot [{{name}}.server]}}
   :main ^{:skip-aot true} {{name}}.server)
