06ec9e69f4652561c4ec3d9c355772759977305e
Matt Rkiouak <mrkiouak@gmail.com>
2020-07-08 09:00:31 -0400

    Refresh templates with dependencies for newer versions of protojure
    Signed-off-by: Matt Rkiouak <mrkiouak@gmail.com>

--- resources/leiningen/new/protojure/project.clj
+++ resources/leiningen/new/protojure/project.clj
@@ -9,17 +9,20 @@
                  [io.pedestal/pedestal.service "0.5.7"]
 
                  ;; -- PROTOC-GEN-CLOJURE --
-                 [protojure "1.2.2"]
+                 [protojure "1.5.2"]
                  [protojure/google.protobuf "0.9.1"]
 
-                 ;; -- PROTOC_GEN_CLOJURE CLIENT DEP --
-                 [org.eclipse.jetty.http2/http2-client "9.4.25.v20191220"]
+                 ;; -- PROTOC_GEN_CLOJURE CLIENT DEPS --
+                 [org.eclipse.jetty.http2/http2-client "9.4.20.v20190813"]
+                 [org.eclipse.jetty/jetty-alpn-java-client "9.4.28.v20200408"]
+                 ;; -- Jetty Client Dep --
+                 [org.ow2.asm/asm "8.0.1"]
 
                  ;; Include Undertow for supporting HTTP/2 for GRPCs
                  [io.undertow/undertow-core "2.0.28.Final"]
                  [io.undertow/undertow-servlet "2.0.28.Final"]
                  ;; And of course, protobufs
-                 [com.google.protobuf/protobuf-java "3.11.1"]
+                 [com.google.protobuf/protobuf-java "3.12.2"]
                  ;; logging
                  [com.taoensso/timbre "4.10.0"]
                  [com.fzakaria/slf4j-timbre "0.3.17"]
