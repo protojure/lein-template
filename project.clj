65eff1c798e580afb7c2ea096aa42ebf20eb123b
Greg Haskins <greg@manetu.com>
2020-07-20 07:54:20 -0400

    Prepare for v1.1.4 development
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
 ;;
 ;; SPDX-License-Identifier: Apache-2.0
 
-(defproject protojure/lein-template "1.1.3"
+(defproject protojure/lein-template "1.1.4-SNAPSHOT"
   :description "A leiningen template for creating protojure projects"
   :url "http://github.com/protojure/lein-template"
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"
-            :year 2019
+            :year 2020
             :key "apache-2.0"}
   :eval-in-leiningen true)
