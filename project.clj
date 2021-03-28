577f122320d0345dfca7b468d093aa8c80cc979c
Greg Haskins <greg@manetu.com>
2021-03-28 13:17:31 -0400

    Release v1.1.5
    Changes since v1.1.4
    ----------------------
    3a9b969 Update to the latest dependencies
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 ;; Copyright © 2019 State Street Bank and Trust Company.  All rights reserved
-;; Copyright © 2019-2020 Manetu, Inc
+;; Copyright © 2019-2021 Manetu, Inc
 ;;
 ;; SPDX-License-Identifier: Apache-2.0
 
-(defproject protojure/lein-template "1.1.5-SNAPSHOT"
+(defproject protojure/lein-template "1.1.5"
   :description "A leiningen template for creating protojure projects"
   :url "http://github.com/protojure/lein-template"
   :license {:name "Apache License 2.0"
