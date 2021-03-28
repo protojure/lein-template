3a9b96917bfdaa3f8be15b7d4bad4987c3721250
Greg Haskins <greg@manetu.com>
2021-03-28 13:07:28 -0400

    Update to the latest dependencies
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- resources/leiningen/new/protojure/project.clj
+++ resources/leiningen/new/protojure/project.clj
@@ -5,27 +5,27 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :year 2019
             :key "apache-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [io.pedestal/pedestal.service "0.5.8"]
 
                  ;; -- PROTOC-GEN-CLOJURE --
-                 [protojure "1.5.11"]
+                 [protojure "1.6.0"]
                  [protojure/google.protobuf "0.9.1"]
 
                  ;; -- PROTOC_GEN_CLOJURE CLIENT DEPS --
-                 [org.eclipse.jetty.http2/http2-client "9.4.35.v20201120"]
-                 [org.eclipse.jetty/jetty-alpn-java-client "9.4.35.v20201120"]
+                 [org.eclipse.jetty.http2/http2-client "11.0.1"]
+                 [org.eclipse.jetty/jetty-alpn-java-client "11.0.1"]
                  ;; -- Jetty Client Dep --
-                 [org.ow2.asm/asm "9.0"]
+                 [org.ow2.asm/asm "9.1"]
 
                  ;; Include Undertow for supporting HTTP/2 for GRPCs
-                 [io.undertow/undertow-core "2.2.3.Final"]
-                 [io.undertow/undertow-servlet "2.2.3.Final"]
+                 [io.undertow/undertow-core "2.2.5.Final"]
+                 [io.undertow/undertow-servlet "2.2.5.Final"]
                  ;; And of course, protobufs
-                 [com.google.protobuf/protobuf-java "3.14.0"]
+                 [com.google.protobuf/protobuf-java "3.15.6"]
                  ;; logging
-                 [com.taoensso/timbre "5.1.0"]
-                 [com.fzakaria/slf4j-timbre "0.3.20"]
+                 [com.taoensso/timbre "5.1.2"]
+                 [com.fzakaria/slf4j-timbre "0.3.21"]
 
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.30"]
