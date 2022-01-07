de1ce252e150304eb23ade27cab5682fb92120a6
Greg Haskins <greg@manetu.com>
2022-01-07 14:33:12 -0500

    Update to protojure v2.0
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 ;; Copyright © 2019 State Street Bank and Trust Company.  All rights reserved
-;; Copyright © 2019-2021 Manetu, Inc
+;; Copyright © 2019-2022 Manetu, Inc
 ;;
 ;; SPDX-License-Identifier: Apache-2.0
 
-(defproject protojure/lein-template "1.1.7-SNAPSHOT"
+(defproject protojure/lein-template "2.0.0-SNAPSHOT"
   :description "A leiningen template for creating protojure projects"
   :url "http://github.com/protojure/lein-template"
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"
-            :year 2020
+            :year 2022
             :key "apache-2.0"}
   :eval-in-leiningen true)
--- resources/leiningen/new/protojure/server.clj
+++ resources/leiningen/new/protojure/server.clj
@@ -2,13 +2,7 @@
   (:gen-class) ; for -main method in uberjar
   (:require [io.pedestal.http :as server]
             [io.pedestal.http.route :as route]
-            [{{name}}.service :as service]
-            [taoensso.timbre :as log]
-            [taoensso.timbre.appenders.core :as appenders]))
-
-(log/set-config! {:level :debug
-                  :ns-whitelist  ["{{name}}.*"]
-                  :appenders {:println (appenders/println-appender {:stream :auto})}})
+            [{{name}}.service :as service]))
 
 ;; This is an adapted service map, that can be started and stopped
 ;; From the REPL you can call server/start and server/stop on this service
