b3215e84fccfb75ba06db02c1af6df885d1d1db2
Greg Haskins <greg@manetu.com>
2022-01-07 14:33:12 -0500

    Update to protojure v2.0
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- resources/leiningen/new/protojure/project.clj
+++ resources/leiningen/new/protojure/project.clj
@@ -3,29 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"
-            :year 2019
+            :year 2022
             :key "apache-2.0"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [io.pedestal/pedestal.service "0.5.9"]
 
                  ;; -- PROTOC-GEN-CLOJURE --
-                 [protojure "1.7.0"]
-                 [protojure/google.protobuf "1.0.0"]
-
-                 ;; -- PROTOC_GEN_CLOJURE CLIENT DEPS --
-                 [org.eclipse.jetty.http2/http2-client "11.0.7"]
-                 [org.eclipse.jetty/jetty-alpn-java-client "11.0.7"]
-                 ;; -- Jetty Client Dep --
-                 [org.ow2.asm/asm "9.1"]
-
-                 ;; Include Undertow for supporting HTTP/2 for GRPCs
-                 [io.undertow/undertow-core "2.2.14.Final"]
-                 [io.undertow/undertow-servlet "2.2.14.Final"]
-                 ;; And of course, protobufs
-                 [com.google.protobuf/protobuf-java "3.19.1"]
-                 ;; logging
-                 [com.taoensso/timbre "5.1.2"]
-                 [com.fzakaria/slf4j-timbre "0.3.21"]
+                 [io.github.protojure/grpc-server "2.0.1"]
+                 [io.github.protojure/google.protobuf "2.0.0"]
 
                  [ch.qos.logback/logback-classic "1.2.9"]
                  [org.slf4j/jul-to-slf4j "1.7.32"]
